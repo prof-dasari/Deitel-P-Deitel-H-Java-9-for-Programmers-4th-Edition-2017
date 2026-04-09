59933f2344e32a35ff4f3e07dfabaed0436a98a4
Dasari <sree91@gmail.com>
2026-04-09 13:22:55 +0530

    Deitel

---
+++ ch15/SerializationApps/Accounts.java
@@ -0,0 +1,36 @@
+// Fig. 15.x: AccountList.java
+// Maintains a List<Account>
+import java.util.ArrayList;
+import java.util.List;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlRootElement(name="accounts")
+public class Accounts {
+   // stores Account objects
+   @XmlElement(name="account")
+   private List<Account> accounts = new ArrayList<>();
+
+   // returns the List<Accounts>
+   public List<Account> getAccounts() {return accounts;}
+
+   public void setAccounts(List<Account> accounts) {this.accounts = accounts;}
+}
+
+/*************************************************************************
+* (C) Copyright 1992-2018 by Deitel & Associates, Inc. and               *
+* Pearson Education, Inc. All Rights Reserved.                           *
+*                                                                        *
+* DISCLAIMER: The authors and publisher of this book have used their     *
+* best efforts in preparing the book. These efforts include the          *
+* development, research, and testing of the theories and programs        *
+* to determine their effectiveness. The authors and publisher make       *
+* no warranty of any kind, expressed or implied, with regard to these    *
+* programs or to the documentation contained in these books. The authors *
+* and publisher shall not be liable in any event for incidental or       *
+* consequential damages in connection with, or arising out of, the       *
+* furnishing, performance, or use of these programs.                     *
+*************************************************************************/
